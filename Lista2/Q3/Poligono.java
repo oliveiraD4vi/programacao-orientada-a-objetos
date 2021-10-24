30789a2d784e715bf6b040b429da49016fa5f4cd
Davi Oliveira <davi.comp.oliveira@gmail.com>
2021-10-24 14:08:27 -0300

    adicionados comentarios para arquivos da questao 3

--- Lista2/Q3/Poligono.java
+++ Lista2/Q3/Poligono.java
@@ -1,4 +1,10 @@
 /**
+ * Para o modelo Poligono os atributos incluem apenas quantidade
+ * de lados e a medida do lado. Dentre as operações temos operações
+ * que retornam o valor de alguns dados que podem ser mostrados na 
+ * operação mostraDados, tais como a quantidade de diagonais, a soma
+ * dos ângulos internos do polígono e a medida do ângulo interno do
+ * polígono.
  * @author Davi Silva Alves de Oliveira
  */
 
@@ -9,11 +15,20 @@ class Poligono {
   int qntdLados;
   int medidaDoLado;
 
+  /**
+   * O método inicializaDados inicializa as variáveis com os dados
+   * passados pelo usuário.
+   * @param lados é o número de lados do polígono
+   * @param medidaLado é a medida do lado do polígono
+   */
   void inicializaDados(int lados, int medidaLado) {
     qntdLados = lados;
     medidaDoLado = medidaLado;
   }
 
+  /**
+   * O método mostraDados imprime os dados de interesse do polígono.
+   */
   void mostraDados() {
     System.out.println();
     mostraNomeDoPoligono();
@@ -26,18 +41,36 @@ class Poligono {
     System.out.println("----------------");
   }
 
+  /**
+   * O método calculaSomaDosAngulosInternos calcula a soma dos angulos internos
+   * a partir da fórmula próŕia.
+   * @return o resultado do cálculo
+   */
   int calculaSomaDosAngulosInternos() {
     return ((qntdLados-2)*180);
   }
 
+  /**
+   * O método calculaNumeroDiagonais calcula o número de diagonais do polígono
+   * a partir da fórmula já conhecida para tal.
+   * @return o resultado do cálculo
+   */
   int calculaNumeroDiagonais() {
     return ((qntdLados*(qntdLados-3)/2));
   }
 
+  /**
+   * O método calculaMedidaDoAnguloInterno calcula a medida do angulo interno 
+   * do polígono a partir do calculo da soma dos angulos internos.
+   * @return valor da medida do angulo interno
+   */
   int calculaMedidaDoAnguloInterno() {
     return (calculaSomaDosAngulosInternos()/qntdLados);
   }
 
+  /**
+   * O método mostraNomeDoPoligono imprime o nome do polígono a partir do número de lados
+   */
   void mostraNomeDoPoligono() {
     switch (qntdLados) {
       case 3:
