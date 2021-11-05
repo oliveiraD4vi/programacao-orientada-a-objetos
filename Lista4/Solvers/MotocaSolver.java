eb059322731aceb6de1b6b2ce1c6967bd43a9167
Davi Oliveira <davi.comp.oliveira@gmail.com>
2021-11-05 15:50:02 -0300

    ajustada identaçao dos solvers

--- Lista4/Solvers/MotocaSolver.java
+++ Lista4/Solvers/MotocaSolver.java
@@ -1,134 +1,135 @@
 public class Person {
-    private String name;
-    private int age;
-    public Person(String name, int age);
-    public String toString();
+  private String name;
+  private int age;
+  public Person(String name, int age);
+  public String toString();
 }
 
 public class Motorcycle {
-    private Person person; //agregacao
-    private int power;
-    private int time;
-    //Inicia o atributo power, time com zero e person com null
-    public Motorcycle(int power);
-    //Comprar mais tempo
-    public void buy(int time);
-    //Se estiver vazio, coloca a pessoa na moto e retorna true
-    public boolean in(Person person);
-    //Se houver uma person, retira e retorna
-    //Se não, retorna null
-    public Person out();
-    public void drive(int time);
-    //buzinar
-    public void honk();
-    public String toString();
+  private Person person; //agregacao
+  private int power;
+  private int time;
+
+  //Inicia o atributo power, time com zero e person com null
+  public Motorcycle(int power);
+  //Comprar mais tempo
+  public void buy(int time);
+  //Se estiver vazio, coloca a pessoa na moto e retorna true
+  public boolean in(Person person);
+  //Se houver uma person, retira e retorna
+  //Se não, retorna null
+  public Person out();
+  public void drive(int time);
+  //buzinar
+  public void honk();
+  public String toString();
 }
 
 public class Solver{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Motorcycle motoca  = new Motorcycle(1);
-        while(true) {
-            String line = scanner.nextLine();
-            String ui[] = line.split(" "); //ui user input eh um vetor de strings
-            System.out.println("$" + line);
-            if(ui[0].equals("end")) {
-                break;
-            }else if(ui[0].equals("init")) { //power
-                motoca = new Motorcycle(Integer.parseInt(ui[1]));
-            }else if(ui[0].equals("in")) { //in name age
-                int age = Integer.parseInt(ui[2]);
-                Person person = new Person(ui[1], age);
-                motoca.in(person);
-            }else if(ui[0].equals("out")) {
-                motoca.out();
-            }else if(ui[0].equals("show")) {
-                System.out.println(motoca);
-            }else if(ui[0].equals("drive")) {
-                motoca.drive(Integer.parseInt(ui[1]));
-            }else if(ui[0].equals("buy")) {
-                motoca.buy(Integer.parseInt(ui[1]));
-            }else if(ui[0].equals("honk")) {
-                motoca.honk();
-            }else {
-                System.out.println("Comando invalido");
-            }
-        }
-        scanner.close();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Motorcycle motoca  = new Motorcycle(1);
+    while(true) {
+      String line = scanner.nextLine();
+      String ui[] = line.split(" "); //ui user input eh um vetor de strings
+      System.out.println("$" + line);
+      if(ui[0].equals("end")) {
+          break;
+      }else if(ui[0].equals("init")) { //power
+          motoca = new Motorcycle(Integer.parseInt(ui[1]));
+      }else if(ui[0].equals("in")) { //in name age
+          int age = Integer.parseInt(ui[2]);
+          Person person = new Person(ui[1], age);
+          motoca.in(person);
+      }else if(ui[0].equals("out")) {
+          motoca.out();
+      }else if(ui[0].equals("show")) {
+          System.out.println(motoca);
+      }else if(ui[0].equals("drive")) {
+          motoca.drive(Integer.parseInt(ui[1]));
+      }else if(ui[0].equals("buy")) {
+          motoca.buy(Integer.parseInt(ui[1]));
+      }else if(ui[0].equals("honk")) {
+          motoca.honk();
+      }else {
+          System.out.println("Comando invalido");
+      }
     }
+    scanner.close();
+  }
 }
 
 public class Manual{
-    public static void main(String[] args) {
-        //case subindo e buzinando
-        Motorcycle moto = new Motorcycle(1);
-        System.out.println(moto);
-        //power: 1, minutos: 0, person: null
-        moto.honk();
-        //fail: moto vazia
-        moto.in(new Person("marcos", 4));
-        System.out.println(moto);
-        //power: 1, minutos: 0, person: [marcos:4]
-        moto.honk();
-        //Pem
-        moto.in(new Person("marisa", 2));
-        //fail: moto ocupada
-        System.out.println(moto);
-        //power: 1, minutos: 0, person: [marcos:4]
+  public static void main(String[] args) {
+    //case subindo e buzinando
+    Motorcycle moto = new Motorcycle(1);
+    System.out.println(moto);
+    //power: 1, minutos: 0, person: null
+    moto.honk();
+    //fail: moto vazia
+    moto.in(new Person("marcos", 4));
+    System.out.println(moto);
+    //power: 1, minutos: 0, person: [marcos:4]
+    moto.honk();
+    //Pem
+    moto.in(new Person("marisa", 2));
+    //fail: moto ocupada
+    System.out.println(moto);
+    //power: 1, minutos: 0, person: [marcos:4]
 
-        //case subindo e buzinando
-        moto = new Motorcycle(5);
-        System.out.println(moto);
-        //power: 5, minutos: 0, person: null
-        moto.in(new Person("marcos", 4));
-        System.out.println(moto);
-        //power: 5, minutos: 0, person: [marcos:4]
-        moto.honk();
-        //Peeeeem
+    //case subindo e buzinando
+    moto = new Motorcycle(5);
+    System.out.println(moto);
+    //power: 5, minutos: 0, person: null
+    moto.in(new Person("marcos", 4));
+    System.out.println(moto);
+    //power: 5, minutos: 0, person: [marcos:4]
+    moto.honk();
+    //Peeeeem
 
-        //case subindo e trocando
-        moto = new Motorcycle(7);
-        moto.in(new Person("heitor", 6));
-        System.out.println(moto);
-        //power: 7, minutos: 0, person: [heitor:6]
-        Person heitor = moto.out();
-        System.out.println(heitor);
-        //[heitor:6]
-        moto.out();
-        //fail: moto vazia
-        moto.in(new Person("suzana", 8));
-        System.out.println(moto);
-        //power: 7, minutos: 0, person: [suzana:8]
+    //case subindo e trocando
+    moto = new Motorcycle(7);
+    moto.in(new Person("heitor", 6));
+    System.out.println(moto);
+    //power: 7, minutos: 0, person: [heitor:6]
+    Person heitor = moto.out();
+    System.out.println(heitor);
+    //[heitor:6]
+    moto.out();
+    //fail: moto vazia
+    moto.in(new Person("suzana", 8));
+    System.out.println(moto);
+    //power: 7, minutos: 0, person: [suzana:8]
 
-        //case passeando
-        moto = new Motorcycle(7);
-        moto.in(new Person("suzana", 8));
-        moto.drive(10);
-        //fail: time zerado
-        moto.buy(40);
-        System.out.println(moto);
-        //power: 7, minutos: 40, person: [suzana:8]
-        moto.drive(20);
-        System.out.println(moto);
-        //power: 7, minutos: 20, person: [suzana:8]
+    //case passeando
+    moto = new Motorcycle(7);
+    moto.in(new Person("suzana", 8));
+    moto.drive(10);
+    //fail: time zerado
+    moto.buy(40);
+    System.out.println(moto);
+    //power: 7, minutos: 40, person: [suzana:8]
+    moto.drive(20);
+    System.out.println(moto);
+    //power: 7, minutos: 20, person: [suzana:8]
 
-        //case nem grande nem pequeno
-        moto = new Motorcycle(7);
-        moto.buy(20);
-        moto.in(new Person("andreina", 23));
-        moto.drive(15);
-        //fail: muito grande para andar de moto
-        System.out.println(moto);
-        //power: 7, minutos: 20, person: [andreina:23]
+    //case nem grande nem pequeno
+    moto = new Motorcycle(7);
+    moto.buy(20);
+    moto.in(new Person("andreina", 23));
+    moto.drive(15);
+    //fail: muito grande para andar de moto
+    System.out.println(moto);
+    //power: 7, minutos: 20, person: [andreina:23]
 
-        //case acabou o time
-        moto = new Motorcycle(7);
-        moto.buy(20);
-        moto.in(new Person("andreina", 6));
-        moto.drive(15);
-        System.out.println(moto);
-        //power: 7, minutos: 5, person: [andreina:6]
-        moto.drive(10);
-        //fail: andou 5 min e acabou o time
-    }
+    //case acabou o time
+    moto = new Motorcycle(7);
+    moto.buy(20);
+    moto.in(new Person("andreina", 6));
+    moto.drive(15);
+    System.out.println(moto);
+    //power: 7, minutos: 5, person: [andreina:6]
+    moto.drive(10);
+    //fail: andou 5 min e acabou o time
+  }
 }
