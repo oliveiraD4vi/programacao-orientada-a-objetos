68dddb5b9ef649342640f8e404c956947054586e
Davi Oliveira <davi.comp.oliveira@gmail.com>
2021-11-25 22:11:07 -0300

    adicionada resoluçao questao trampoline

--- Lista6/PulaPula/Kid.java
+++ Lista6/PulaPula/Kid.java
@@ -1,10 +1,53 @@
 class Kid {
   private int age;
   private String name;
-  public Kid(String name, int age);
-  int getAge();
-  String getName();
-  void setAge(int age);
-  public void setName(String name); //gs
-  public String toString();
+
+  /**
+   * 
+   * @param name
+   * @param age
+   */
+  public Kid(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  /**
+   * 
+   * @return
+   */
+  int getAge() {
+    return age;
+  }
+  
+  /**
+   * 
+   * @return
+   */
+  String getName() {
+    return name;
+  }
+  
+  /**
+   * 
+   * @param age
+   */
+  void setAge(int age) {
+    this.age = age;
+  }
+  
+  /**
+   * 
+   * @param name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  /**
+   * 
+   */
+  public String toString() {
+    return (name + ":" + age);
+  }
 }
--- Lista6/PulaPula/Trampoline.java
+++ Lista6/PulaPula/Trampoline.java
@@ -1,16 +1,73 @@
+import java.util.LinkedList;
+
 class Trampoline{
-  private LinkedList<Kid> waiting;
-  private LinkedList<Kid> playing;
-  public Trampoline();
-  //remove and return the Kid with this name or null
-  private Kid remove_kid(String name, LinkedList<Kid> list);
-  //insere na lista de espera
-  public void arrive(Kid kid);
-  //remove da lista de espera e insere na lista playing
-  public void in();
-  //remove de playing para wainting
-  public void out();
-  //remove do parquinho
-  public Kid remove(String name);
-  public String toString();
+  private LinkedList<Kid> waiting = new LinkedList<Kid>();
+  private LinkedList<Kid> playing = new LinkedList<Kid>();
+
+  /**
+   * 
+   */
+  public Trampoline() {}
+
+  /**
+   * 
+   * @param name
+   * @param list
+   * @return
+   */
+  private Kid remove_kid(String name, LinkedList<Kid> list) {
+    Kid kid = new Kid(name, 10);
+
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).getName() == name) {
+        kid = list.get(i);
+        list.remove(kid);
+        break;
+      }
+    }
+
+    return kid;
+  }
+  
+  /**
+   * 
+   * @param kid
+   */
+  public void arrive(Kid kid) {
+    waiting.addFirst(kid);
+  }
+  
+  /**
+   * 
+   */
+  public void in() {
+    playing.addFirst(waiting.getLast());
+    waiting.removeLast();
+  }
+  
+  /**
+   * 
+   */
+  public void out() {
+    waiting.addFirst(playing.getLast());
+    playing.removeLast();
+  }
+  
+  /**
+   * 
+   * @param name
+   * @return
+   */
+  public Kid remove(String name) {
+    Kid kid = new Kid("undefined", 0);
+
+    kid = remove_kid(name, waiting);
+    if (kid.getName() == "undefined") remove_kid(name, playing);
+
+    return kid;
+  }
+  
+  public String toString() {
+    return ("=> " + waiting + " => " + playing);
+  }
 }
